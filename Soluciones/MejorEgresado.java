7086494d30d8136708500d106f3d0acc502b5fe7
Jeank935 <jccuenca17@utpl.edu.ec>
2023-12-02 19:55:27 -0500

    Soluciones Jean

---
+++ Soluciones/MejorEgresado.java
@@ -0,0 +1,30 @@
+package mejoregresado;
+
+import java.util.Scanner;
+
+public class MejorEgresado {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numEstudiantes, cont;
+        double promedio, promMayor = 0;
+        String nombres = null, nombre;//Iniciaializamos las variables string en null que es el equivalente de 0 en string
+        System.out.println("Cuantos estudiantes desea ingresar:");//Ingrese la cantidad de estudiantes
+        numEstudiantes = teclado.nextInt();
+        for (cont = 0; cont < numEstudiantes; cont++) {//Se utiliza ciclo repetitivo for
+            System.out.println("Ingrese el nombre del estudiante: ");
+            nombre = teclado.next();
+            System.out.println("Ingrese el promedio final del estudiante: ");
+            promedio = teclado.nextDouble();
+            if (promedio == promMayor) {
+                nombres += " y " + nombre; // Si hay más de un estudiante con el promedio más alto
+            } else if (promedio > promMayor) {
+                promMayor = promedio; // Se asigna el mayor promedio
+                nombres = nombre;
+            }
+        }
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        System.out.println("DEBIDO A SU GRAN ESFUERZO Y DEDICACION SE LE OTORGA UN RECONOCIMIENTO AL MEJOR EGRESADO" + nombres + "QUE OBTUVO UN PROMEDIO DE:" + promMayor);
+    }
+}
+
